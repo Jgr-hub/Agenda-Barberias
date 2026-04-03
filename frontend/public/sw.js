78b64acf8c28ca90389025c0b5555a3fa708dd5a
Jgr-hub <jhomergomez1@gmail.com>
2026-04-02 23:22:16 -0500

    fix: desinstalar service worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,30 +1,10 @@
-// Service Worker - Agenda Barberias
-const CACHE_NAME = 'agenda-barberias-v2'
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting()
-})
-
+// Desinstalar Service Worker
+self.addEventListener('install', () => self.skipWaiting())
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+      .then(() => self.registration.unregister())
   )
-})
-
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {}
-  const title = data.title || '¡Nueva cita!'
-  const options = {
-    body: data.body || 'Tienes una nueva reserva',
-    icon: '/favicon.ico',
-    requireInteraction: true,
-  }
-  event.waitUntil(self.registration.showNotification(title, options))
-})
-
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close()
-  event.waitUntil(clients.openWindow('/'))
 })
